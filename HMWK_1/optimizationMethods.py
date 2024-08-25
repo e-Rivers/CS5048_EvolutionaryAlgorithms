b3b4c99e3a19703b39d6d328bed7981edc3e13a1
denisseram <a01562077@tec.mx>
2024-08-25 13:10:47 -0600

    add hill climbing method
    it's not complete yet

--- HMWK_1/optimizationMethods.py
+++ HMWK_1/optimizationMethods.py
@@ -53,7 +53,8 @@ def gradientDescent(t, x, func, tol, *args):
 
 
 ##############################################################
-# Newton's Method
+##################### NEWTON'S METHOD ########################
+##############################################################
 def newtonMethod(t, x, func, tol, *args):
 
     variables = args 
@@ -82,3 +83,54 @@ def newtonMethod(t, x, func, tol, *args):
 
     return solHistory
 
+
+##############################################################
+####################### HILL CLIMBING ########################
+##############################################################
+
+def hill_climbing(t, x, func, tol, *args):
+
+    # t param : 
+    # x param :
+    # func param :
+    # tol param :
+    # args param : 
+
+    variables = args
+    
+    # Start point
+    #agragar lo de que regrese la lista de soluciones
+    current_solution = [0.5,1]
+    current_value = objective_functionb(current_solution[0], current_solution[1])
+    
+    step_size = 0.01
+    max_iterations = 1000
+
+    #counter to see in how many iterations the solution is reached
+    c = 0
+
+    for _ in range(max_iterations):
+        x, y = current_solution
+        
+        # Generate neighbors by adjusting each variable separately + -
+        neighbors = [
+            [x + step_size, y],
+            [x - step_size, y],
+            [x, y + step_size],
+            [x, y - step_size]
+        ]
+        
+        # Evaluate neighbors and select the one that will give the maximum function
+        next_solution = min(neighbors, key=lambda sol: objective_functionb(sol[0], sol[1]))
+        next_value = objective_functionb(next_solution[0], next_solution[1])
+        
+        # Check if the neighbor is better
+        if next_value < current_value:
+            current_solution = next_solution
+            current_value = next_value
+            c += 1
+        else:
+            # If no better neighbors, return current solution
+            break
+
+    return current_solution, current_value, c
