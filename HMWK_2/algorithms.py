55835510d6aaecdd14a4c445a37cee782f289bb1
Emilio Rivers <rivers.emilio2001@protonmail.com>
2024-09-23 15:36:33 -0600

    Moving main to... well, main

--- HMWK_2/algorithms.py
+++ HMWK_2/algorithms.py
@@ -9,78 +9,15 @@ import matplotlib.pyplot as plt
 import csv
 
 
-# Father class: GeneticAlgorithm
-class GeneticAlgorithm(ABC):
-
-    #    def __init__(self, popu_size, num_generations, ind_size, Pc=0.9, Pm=0.1):
-    def __init__(self, lowerBound, upperBound, func, Pc=0.9, Pm=0.1):
-        self._xU = upperBound
-        self._xL = lowerBound
-        self._func = func
-        self._population = None
-        #        self._pop_size = popu_size
-        #        self._num_generations = num_generations
-        #        self._ind_size = ind_size
-        #        self._population = self.initialize_population()
-        #        self._Pc = Pc   # Crossover Probability  
-        #        self._Pm = Pm   # Mutation Probability  
-
-    @abstractmethod
-    def initialize_population(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def run(self, verbose=False):
-        """# Step 1. Initialize Population
-        self._population = self.initialize_population()
-
-        # Step 2. Form the couples for creating children
-        couples = []
-        for _ in range(len(self._population)//2):
-            couples.append((
-                    self._selection()
-                    self._selection()
-                ))
-
-        # Step 3. Perform crossover only for couples with that probability (Pc)
-        newPopulation = []
-        for couple in couples:
-            crossProb = np.random.uniform()
-            if crossProb <= self._Pc:
-                newPopulation.extend(self._crossover(*couple))
-            else:
-                newPopulation.extend(couple)
-
-"""
-        raise NotImplementedError
-
-    def _getFitness(self, individual):
-        return self._func(individual)
-
-    @abstractmethod
-    def _crossover(self, parent1, parent2):
-        raise NotImplementedError
-
-    @abstractmethod
-    def _mutation(self, individual):
-        raise NotImplementedError
-    
-    @abstractmethod
-    def _selection(self):
-        raise NotImplementedError
-
-
-    
-
 
 #####################################################################
 ################## Child class for Binary Encoding ##################
 #####################################################################
 
-class BinaryGA(GeneticAlgorithm):
+class BinaryGA():
 
     def __init__(self, lowerBound, upperBound, func, Pc= 0.9,):
-        super().__init__(lowerBound, upperBound, func, Pc)
+        self._func = func
         
 
     def run(self, popu_size, num_generations, bounds):
@@ -378,12 +315,15 @@ class BinaryGA(GeneticAlgorithm):
 ############# Child class for Real Encoding #################
 #############################################################
 
-class RealGA(GeneticAlgorithm):
+class RealGA():
 
     def __init__(self, lowerBound, upperBound, func, nc = 20, np = 20):
-        super().__init__(lowerBound, upperBound, func)
+        self._xL = lowerBound
+        self._xU = upperBound
+        self._func = func
         self._nc = nc
         self._np = np
+        self._eta_m = 20
 
     # Binary tournament selection
     def _selection(self):
@@ -418,7 +358,7 @@ class RealGA(GeneticAlgorithm):
         return H1, H2
 
     # Parameter-based mutation
-    def _mutation(self, individual, t, uTest = None, iTest = None):
+    def _mutation(self, individual, uTest = None, iTest = None):
         # Step 0. Randomly select the gene to be mutated
         i = np.random.uniform() if iTest == None else iTest
         gene = individual[i]
@@ -427,7 +367,7 @@ class RealGA(GeneticAlgorithm):
         u = np.random.uniform() if uTest == None else uTest
 
         # Step 2. Compute delta sub q
-        eta_m = 100 + t
+        eta_m = self._eta_m # This actually is calculated as 100 + t, where t = generation num. But for the hwmk, it was required as 20
         delta = min((gene - self._xL), (self._xU - gene)) / (self._xU - self._xL)
         if u <= 0.5:
             delta_q = (2*u + (1-2*u)*(1-delta)**(eta_m+1))**(1 / (eta_m+1)) - 1
@@ -441,141 +381,29 @@ class RealGA(GeneticAlgorithm):
 
         return individual 
 
+    def run(self):
+        # Step 1. Initialize Population
+        self._population = self.initialize_population()
+
+        # Step 2. Form the couples for creating children
+        couples = []
+        for _ in range(len(self._population)//2):
+            couples.append((
+                    self._selection(),
+                    self._selection()
+                ))
+
+        # Step 3. Perform crossover only for couples with that probability (Pc)
+        newPopulation = []
+        for couple in couples:
+            crossProb = np.random.uniform()
+            if crossProb <= self._Pc:
+                newPopulation.extend(self._crossover(*couple))
+            else:
+                newPopulation.extend(couple)
+
+    def _getFitness(self, individual):
+        return self._func(individual)
+
     def __str__(self):
         return "Real Encoding"
-
-##################################
-##################################
-###### MAIN PRUEBA ###############
-##################################
-##################################
-
-if __name__ == "__main__":
-
-    def run_experiments(ga_class, problems, pop_size, num_generations, num_runs):
-        results = []
-        fitness_history = []
-        for func, bounds, description, initial_guess in problems:
-            for _ in range(num_runs):
-                # Initialize GA with the bounds and function
-                lower_bound, upper_bound = bounds  # Extract min and max bounds
-                ga = ga_class(lowerBound=lower_bound, upperBound=upper_bound, func=func)
-                
-                # Run GA
-                best_fitness = ga.run(popu_size=pop_size, num_generations=num_generations, 
-                                    bounds=[(lower_bound, upper_bound)] * len(initial_guess))
-                
-                # Get the best fitness value in this run
-                best_index = np.argmin(best_fitness)
-                #print(f"Best of generation: {best_index}")
-                results.append(best_fitness[best_index])
-                fitness_history.append(best_fitness)
-        
-        return results, fitness_history
-
-    # Initialize problems
-    problems = [
-    (
-        lambda x: 100*(x[0]**2 - x[1])**2 + (1 - x[0])**2,
-        np.array([-2.048, 2.048]).astype(float),
-        "Function A",
-        np.array([0, 0]).astype(float) 
-    ),
-    (
-        lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
-        np.array([-5.12, 5.12]).astype(float),
-        "Rastrigin (n=2)",
-        np.array([0, 0]).astype(float)
-    ),
-    (
-        lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
-        np.array([-5.12, 5.12]).astype(float),
-        "Rastrigin (n=5)",
-        np.array([0, 0]).astype(float)
-    )
-]
-
-    num_runs = 20
-    results = {}
-    for ga_class in [BinaryGA]: #aqui nomas agregamos la otra clase
-        results[ga_class.__name__] = {}
-        for problem in problems:
-            fitnesses, fitness_history = run_experiments(ga_class, [problem], pop_size=10, num_generations=250, num_runs=num_runs)
-            fitnesses = np.array(fitnesses, dtype=float)
-            results[ga_class.__name__][problem[2]] = {
-                'mean': np.mean(fitnesses),
-                'std': np.std(fitnesses),
-                'min': np.min(fitnesses),
-                'max': np.max(fitnesses),
-                'fitness_history' : fitness_history,
-                'results' : fitnesses
-            }
-
-    # Print results
-    for ga_name, res in results.items():
-        print(f"{ga_name}:")
-        for prob_name, stats in res.items():
-            print(f"  {prob_name}: Mean: {stats['mean']}, Std Dev: {stats['std']} ,Min: {stats['min']}, Max: {stats['max']}")
-            print(f"  {prob_name}: 20 experiments: {stats['results']}")
-
-#### for the table
-    rows = []
-    historial = []
-    auxi = []
-
-    # Recopilar los resultados en formato de tabla
-    for ga_name, res in results.items():
-        for prob_name, stats in res.items():
-            # Crear una fila básica con los stats
-            row = [
-                ga_name,
-                prob_name,
-                stats['mean'],
-                stats['std'],
-                stats['min'],
-                stats['max']
-            ]
-            
-            # Agregar los valores de "20 experiments" como columnas adicionales
-            experiments = stats['results']  # Asegúrate de que esto sea una lista
-            row.extend(experiments)
-            
-            # Añadir la fila completa a las filas
-            rows.append(row)
-
-            historial_20_experiments = stats['fitness_history']
-            historial.append(historial_20_experiments)
-
-            name=[ga_name,prob_name]
-            auxi.append(name)
-
-
-    with open('resultados.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        print(len(stats['results']))
-        header = ['GA Name', 'Problem Name', 'Mean', 'Std Dev', 'Min', 'Max'] + [f'Experiment {i+1}' for i in range(len(stats['results']))]
-        writer.writerow(header)
-        
-        writer.writerows(rows)
-
-##### plot random
-    #select a random experiment
-    r = random.randint(0, len(historial[0])-1)
-    print(auxi)
-    # select and plot the history of those 
-    y = np.arange(1, len(historial[0][0])+1)
-    cols_plot = 3
-    rows_plot = 1
-    plt.figure(figsize=(10, 5 * rows_plot))
-
-    for i in range(len(historial)) :
-        plt.subplot(rows_plot, cols_plot, i + 1)  # Crear un subplot
-        plt.plot(y, historial[i][r], linestyle='-', color='b')
-        plt.title(f'{auxi[i][1]}')
-        plt.xlabel('Number of generations')
-        plt.ylabel(f'Fitness value')
-        plt.grid()
-        
-    plt.tight_layout()  # Ajustar el layout
-    plt.show()
-    print(len(historial[0]))
--- HMWK_2/main.py
+++ HMWK_2/main.py
@@ -4,108 +4,130 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Definition of the functions
-# Each element in the list represents a function and contains the following elements:
-# 1. The mathematical expression of the function.
-# 2. The constraints for the variables.
-# 3. A string with the name of the function.
-# 4. The known or expected optimal solution, represented as a NumPy array.
+def run_experiments(ga_class, problems, pop_size, num_generations, num_runs):
+    results = []
+    fitness_history = []
+    for func, bounds, description, initial_guess in problems:
+        for _ in range(num_runs):
+            # Initialize GA with the bounds and function
+            lower_bound, upper_bound = bounds  # Extract min and max bounds
+            ga = ga_class(lowerBound=lower_bound, upperBound=upper_bound, func=func)
+            
+            # Run GA
+            best_fitness = ga.run(popu_size=pop_size, num_generations=num_generations, 
+                                bounds=[(lower_bound, upper_bound)] * len(initial_guess))
+            
+            # Get the best fitness value in this run
+            best_index = np.argmin(best_fitness)
+            #print(f"Best of generation: {best_index}")
+            results.append(best_fitness[best_index])
+            fitness_history.append(best_fitness)
+    
+    return results, fitness_history
+
+# Initialize problems
 problems = [
-    (
-        lambda x: 100*(x[0]**2 - x[1])**2 + (1 - x[0])**2,
-        np.array([-2.048, 2.048]).astype(float),
-        "Function A",
-        np.array([0, 0]).astype(float) 
-    ),
-    (
-        lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
-        np.array([-5.12, 5.12]).astype(float),
-        lambda n: f"Rastrigin (n={n})",
-        np.array([0, 0]).astype(float)
-    )
+(
+    lambda x: 100*(x[0]**2 - x[1])**2 + (1 - x[0])**2,
+    np.array([-2.048, 2.048]).astype(float),
+    "Function A",
+    np.array([0, 0]).astype(float) 
+),
+(
+    lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
+    np.array([-5.12, 5.12]).astype(float),
+    "Rastrigin (n=2)",
+    np.array([0, 0]).astype(float)
+),
+(
+    lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
+    np.array([-5.12, 5.12]).astype(float),
+    "Rastrigin (n=5)",
+    np.array([0, 0]).astype(float)
+)
 ]
 
-"""
-# Create an empty array to store the results of the implementation of the methods
-solutions = []
 
-# Evaluate the functions
-# For each problem, test all the solution methods
-for function, _, _, _ in functions:
-    for GenAlg, _ in geneticAlgorithms:
+num_runs = 20
+results = {}
+for ga_class in [BinaryGA]: #aqui nomas agregamos la otra clase
+    results[ga_class.__name__] = {}
+    for problem in problems:
+        fitnesses, fitness_history = run_experiments(ga_class, [problem], pop_size=10, num_generations=250, num_runs=num_runs)
+        fitnesses = np.array(fitnesses, dtype=float)
+        results[ga_class.__name__][problem[2]] = {
+            'mean': np.mean(fitnesses),
+            'std': np.std(fitnesses),
+            'min': np.min(fitnesses),
+            'max': np.max(fitnesses),
+            'fitness_history' : fitness_history,
+            'results' : fitnesses
+        }
 
-        genAlg = GenAlg()
-        genAlg.initialize_population()
+# Print results
+for ga_name, res in results.items():
+    print(f"{ga_name}:")
+    for prob_name, stats in res.items():
+        print(f"  {prob_name}: Mean: {stats['mean']}, Std Dev: {stats['std']} ,Min: {stats['min']}, Max: {stats['max']}")
+        print(f"  {prob_name}: 20 experiments: {stats['results']}")
 
-        # HERE GOES THE SOLVING PROCESS
+rows = []
+historial = []
+auxi = []
 
-        # The resulting solution (a list of points) is appended to the solutions list.
-        solutions.append(None)
-
-# Export the results to a csv file
-with open("report.txt", "w") as report:
-    for i in range(len(functions)):
-        report.write(f"⦿ {functions[i][2]}\n")
-        report.write(f"- Real Evaluation: {fx[i](*functions[i][3])}\n")
-        for j in range(len(methods)):
-            report.write(f"\t◙ {methods[j][1]}\n")
-            report.write(f"\t\t- Point Found: {(point := solutions[i*3 +j][-1])}\n")
-            report.write(f"\t\t- Evaluation: {fx[i](*point)}\n")
-            report.write(f"\t\t- Iterations: {len(solutions[i*3 +j])}\n")
-            report.write(f"\t\t- Two Norm Error: {np.linalg.norm(solutions[i*3 + j][-1] - functions[i][3])}\n")
-        report.write("\n\n")
+# Recopilar los resultados en formato de tabla
+for ga_name, res in results.items():
+    for prob_name, stats in res.items():
+        # Crear una fila básica con los stats
+        row = [
+            ga_name,
+            prob_name,
+            stats['mean'],
+            stats['std'],
+            stats['min'],
+            stats['max']
+        ]
         
-# Creation of a grid from -6 to 6 for plotting of the contour plots
-x, y = np.meshgrid((linspace := np.linspace(-6, 6, 200)), linspace)
+        # Agregar los valores de "20 experiments" como columnas adicionales
+        experiments = stats['results']  # Asegúrate de que esto sea una lista
+        row.extend(experiments)
+        
+        # Añadir la fila completa a las filas
+        rows.append(row)
 
-# Create a grid of 3x3 (rows are the problems/functions & cols are the solution methods)
-fig, ax = plt.subplots(3, 3, figsize=(12, 8))
-fig.suptitle("Contour Plots & Solutions Reached")
+        historial_20_experiments = stats['fitness_history']
+        historial.append(historial_20_experiments)
 
-for i in range(len(functions)):
-    for j in range(len(methods)):
-        # Plot the contour plot based on the points in the grid and the function
-        contourPlot = ax[i, j].contour(x, y, fx[i](x, y), levels=20)
-        # Plot the solution found by each method (all the points of all iterations)
-        ax[i, j].plot(*zip(*solutions[i*3 + j]), marker=".", linestyle="-", color="red")
-    ax[i, 0].set_ylabel(functions[i][2])
+        name=[ga_name,prob_name]
+        auxi.append(name)
 
-for i in range(len(functions)):
-    ax[0, i].set_title(methods[i][1])
 
-fig.tight_layout()
+with open('resultados.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    print(len(stats['results']))
+    header = ['GA Name', 'Problem Name', 'Mean', 'Std Dev', 'Min', 'Max'] + [f'Experiment {i+1}' for i in range(len(stats['results']))]
+    writer.writerow(header)
+    
+    writer.writerows(rows)
+
+# plot random
+#select a random experiment
+r = random.randint(0, len(historial[0])-1)
+print(auxi)
+# select and plot the history of those 
+y = np.arange(1, len(historial[0][0])+1)
+cols_plot = 3
+rows_plot = 1
+plt.figure(figsize=(10, 5 * rows_plot))
+
+for i in range(len(historial)) :
+    plt.subplot(rows_plot, cols_plot, i + 1)  # Crear un subplot
+    plt.plot(y, historial[i][r], linestyle='-', color='b')
+    plt.title(f'{auxi[i][1]}')
+    plt.xlabel('Number of generations')
+    plt.ylabel(f'Fitness value')
+    plt.grid()
+    
+plt.tight_layout()  # Ajustar el layout
 plt.show()
-
-"""
-
-if __name__ == "__main__":
-    # Ask for input variables
-    print("GENETIC ALGORITHMS")
-    print("Would you like to setup the parameters manually or go with the default values?")
-    # WE CAN CHANGE THIS DEFAULT VALUES LATER
-    print("Default population size: 4") 
-    print("Default number of generations: 100")
-    setup = input("\nGo with default values? [y/n] ")
-    populationSize = 4 if setup == "y" else float(input("Set a population size: "))
-    maxGenerations = 100 if setup == "y" else int(input("Set the maximum number of generations: "))
-
-    resultChart = pd.DataFrame()
-
-    # Iterate through the 3 problems (p is the index of the problem)
-    for probNum in range(3):
-        p = 1 if probNum > 1 else 0
-
-
-        # Fill the chart with the results of the 20 executions
-        resultChart[name if not callable((name := problems[probNum-p][2])) else name([2, 5][p])] = 0
-
-        problem = problems[p]
-
-        realGA = RealGA(*problem[1])
-
-
-
-
-    print(resultChart)
-
-
+print(len(historial[0]))
