caeff94f0b4015429a89eff6e1e1022bcb47d0cd
denisseram <a01562077@tec.mx>
2024-09-22 18:38:09 -0600

    It works with the three functions!

--- HMWK_2/algorithms.py
+++ HMWK_2/algorithms.py
@@ -29,7 +29,7 @@ class GeneticAlgorithm(ABC):
 
     @abstractmethod
     def run(self, verbose=False):
-        # Step 1. Initialize Population
+        """# Step 1. Initialize Population
         self._population = self.initialize_population()
 
         # Step 2. Form the couples for creating children
@@ -49,6 +49,8 @@ class GeneticAlgorithm(ABC):
             else:
                 newPopulation.extend(couple)
 
+"""
+        raise NotImplementedError
 
     def _getFitness(self, individual):
         return self._func(individual)
@@ -79,18 +81,17 @@ class BinaryGA(GeneticAlgorithm):
         super().__init__(lowerBound, upperBound, func, Pm)
         
 
-    def run(self, popu_size, num_generations, x1_min, x1_max, x2_min, x2_max):
+    def run(self, popu_size, num_generations, bounds):
         # Initialize population
-        num_bits_1 = self.number_bits(x1_min, x1_max)
-        num_bits_2 = self.number_bits(x2_min, x2_max)
-        first_population = self.initialize_population(num_bits_1, num_bits_2, popu_size)
+        num_bits = [self.number_bits(b[0], b[1]) for b in bounds]
+        first_population = self.initialize_population(num_bits, popu_size)
         population = list(first_population)
-        best_solutions =[]
+        best_solutions = []
 
 
         for generation in range(num_generations):
             # Decode population
-            decoded_population = self.decoder(population, x1_min, x2_min, x1_max, x2_max, num_bits_1, num_bits_2)
+            decoded_population = self.decoder(population, bounds, num_bits)
             #print(decoded_population)
         
             
@@ -125,105 +126,65 @@ class BinaryGA(GeneticAlgorithm):
 
     def number_bits(self, x_min, x_max):
         """
-        Determine the number of bits needed to encode the solution
-
-        Input:
-        x_min: Minimum value that the solution can take
-        x_max: Maximum value that the solution can take
-
-        ----------
-
-        Output:
-            if
-        num_bits: Represents the length of the solution
+        Determine the number of bits needed to encode the solution.
+        
+        Arguments:
+        x_min -- Minimum value that the solution can take
+        x_max -- Maximum value that the solution can take
+        
+        Returns:
+        num_bits -- Represents the length of the solution
         """
-        # we calculate the length of the chromosomes needed to encode the solution using 4 numbers after the decimal point
-        num_bits = (x_max - x_min)*10**4
-        num_bits = math.log(num_bits,2)
+        num_bits = (x_max - x_min) * 10**4
+        num_bits = math.log(num_bits, 2)
         num_bits = math.floor(num_bits + 0.99)
         return num_bits
 
-    def initialize_population(self, num_bits_1, num_bits_2, popu_size):
+    def initialize_population(self, num_bits, popu_size):
         """
-        Function to initialize population by encoding variables 
-
-        Input:
-        num_bits_1: number of bits needed to encode x1
-        num_bits_2: number of bits to encode x2
-        popu_size: number of individuals (genomes or chromosomes)
-
-        ----------
-
-        output:
-        population: list of randomly generated solutions
+        Function to initialize the population by encoding variables.
+        
+        Arguments:
+        num_bits -- List of bits needed to encode each variable
+        popu_size -- Number of individuals (genomes or chromosomes)
+        
+        Returns:
+        population -- List of randomly generated solutions
         """
-
-        # Strings to store the binary encoding for each variable (x1 and x2)
-        genome1 = ""
-        genome2 = ""
         population = []
-        # We will do the same procedure for each individual on the population
-        for i in range(popu_size):
-            for j in range(num_bits_1):
-                gene = random.randint(0,1)
-                gene = str(gene)
-                genome1 += gene
-            for k in range(num_bits_2):
-                gene = random.randint(0,1)
-                gene = str(gene)
-                genome2 += gene
-            chromosome = genome1 + genome2
+        # Initialize each individual in the population
+        for _ in range(popu_size):
+            chromosome = ''.join(''.join(str(random.randint(0, 1)) for _ in range(bits)) for bits in num_bits)
             population.append(chromosome)
-            genome1 = ""
-            genome2 = ""
-
         return population
 
     
-    def decoder(self, population, x1_min, x2_min, x1_max, x2_max, num_bits_1, num_bits_2):
-
+    def decoder(self, population, bounds, num_bits):
         """
-        Function to decode from binary numbers to real ones
-
-        Input:
-        population 
-        x1_min : Minimum possible value for x1
-        x2_min : Minimum possible value for x2
-        x1_max : Maximum possible value for x1
-        x2_max : Maximum possible value for x1
-        num_bits_1 : Number of bits needed to encode x1
-        num_bits_2 : Number of bits needed to encode x2
-
-        ----------
-
-        Output:
-        decode_population: list of the real values for each individual (chromosome = [x1, x2])
-
-
+        Function to decode from binary numbers to real ones.
+        
+        Arguments:
+        population -- List of binary encoded individuals
+        bounds -- List of tuples representing min and max bounds for each variable
+        num_bits -- List of bits for each variable
+        
+        Returns:
+        decode_population -- List of decoded real values for each individual
         """
-
-        # Empty list to store the decoded population
         decode_population = []
-
-        # for each chromosome
-        for i in range(len(population)):
-            full_chromosome = population[i]
-            #extract the genome for x1 and x2
-            genome_x1 = full_chromosome[:num_bits_1]
-            genome_x2 = full_chromosome[num_bits_1:]
-
-            #convert the binary string for each variable to a decimal integer
-            x1_deci = int(genome_x1, 2)
-            x2_deci = int(genome_x2, 2)
-
-            #scale the decimal integer to its original range
-            x1 = x1_min + x1_deci * ((x1_max - x1_min)/ (2**num_bits_1 - 1))
-            x2 = x2_min + x2_deci * ((x2_max - x2_min) / (2**num_bits_2 - 1))
-            x2=round(x2,4)
-            x1=round(x1,4)
-            decode_population.append([x1,x2])
-
-
+        for individual in population:
+            values = []
+            pos = 0
+            # Decode each variable
+            for idx, bits in enumerate(num_bits):
+                genome = individual[pos:pos+bits]
+                decimal_value = int(genome, 2)
+                min_bound, max_bound = bounds[idx]
+                # Scale the decimal value to its original range
+                real_value = min_bound + decimal_value * ((max_bound - min_bound) / (2**bits - 1))
+                values.append(round(real_value, 4))
+                pos += bits
+            decode_population.append(values)
         return decode_population
 
 
@@ -242,9 +203,8 @@ class BinaryGA(GeneticAlgorithm):
         """
 
         fit_list = []
-        #x1, x2 = variables
-        for i in range(len(decode_population)):
-            current_value = func.subs({x1: decode_population[i][0], x2: decode_population[i][1]}).evalf()
+        for i in decode_population:
+            current_value = func(i)
             fit_list.append(current_value)
             
 
@@ -468,45 +428,55 @@ class RealGA(GeneticAlgorithm):
 
 if __name__ == "__main__":
 
-
     def run_experiments(ga_class, problems, pop_size, num_generations, num_runs):
         results = []
-        for problem in problems:
+        for func, bounds, description, initial_guess in problems:
+            print(f"Running {description}...")
             for _ in range(num_runs):
-                ga = ga_class(lowerBound=problem[1][0], upperBound=problem[1][1], func=problem[0])
+                # Initialize GA with the bounds and function
+                lower_bound, upper_bound = bounds  # Extract min and max bounds
+                ga = ga_class(lowerBound=lower_bound, upperBound=upper_bound, func=func)
+                
+                # Run GA
                 best_fitness = ga.run(popu_size=pop_size, num_generations=num_generations, 
-                                                                        x1_min=problem[1][0], x1_max=problem[1][1], 
-                                                                        x2_min=problem[1][0], x2_max=problem[1][1])
+                                    bounds=[(lower_bound, upper_bound)] * len(initial_guess))
+                
+                # Get the best fitness value in this run
                 best_index = np.argmin(best_fitness)
-                print(best_fitness)
-                fitness = list(best_fitness)
-                results.append(fitness[best_index])
+                print(f"Fitness values: {best_fitness}")
+                results.append(best_fitness[best_index])
+        
         return results
 
     # Initialize problems
-    x1, x2 = symbols("x1 x2")
     problems = [
-        (
-            100*(x1**2 - x2)**2 + (1 - x1)**2,
-            np.array([-2.048, 2.048]).astype(float),
-            "Function A",
-            np.array([0, 0]).astype(float) 
-        ),
-
-        (
-            (4-2.1*x1**2+((x1**4)/3))*x1**2+x1*x2+(-4+4*x2**2)*x2**2,
-            np.array([0.5, 1]).astype(float),
-            "Function B",
-            np.array([0.08984201774712433, 0.7126563947865581]).astype(float)
-        ),
-    ]
+    (
+        lambda x: 100*(x[0]**2 - x[1])**2 + (1 - x[0])**2,
+        np.array([-2.048, 2.048]).astype(float),
+        "Function A",
+        np.array([0, 0]).astype(float) 
+    ),
+    (
+        lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
+        np.array([-5.12, 5.12]).astype(float),
+        "Rastrigin (n=2)",
+        np.array([0, 0]).astype(float)
+    ),
+    (
+        lambda x: 10*len(x) + sum([(x_i**2 - 10*np.cos(2*np.pi*x_i)) for x_i in x]),
+        np.array([-5.12, 5.12]).astype(float),
+        "Rastrigin (n=5)",
+        np.array([0, 0]).astype(float)
+    )
+]
 
     num_runs = 20
     results = {}
-    for ga_class in [BinaryGA]:
+    for ga_class in [BinaryGA]: #aqui nomas agregamos la otra cosa
         results[ga_class.__name__] = {}
         for problem in problems:
-            fitnesses = run_experiments(ga_class, [problem], pop_size=4, num_generations=10, num_runs=num_runs)
+            fitnesses = run_experiments(ga_class, [problem], pop_size=4, num_generations=100, num_runs=num_runs)
+            fitnesses = np.array(fitnesses, dtype=float)
             results[ga_class.__name__][problem[2]] = {
                 'mean': np.mean(fitnesses),
                 'std': np.std(fitnesses),
@@ -521,4 +491,3 @@ if __name__ == "__main__":
             print(f"  {prob_name}: Mean: {stats['mean']}, Std Dev: {stats['std']} ,Min: {stats['min']}, Max: {stats['max']}")
 
 
-    # You would call plot_convergence after running your experiments
